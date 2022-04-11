37caa31ffc229f7caca0c7188f5910fe9a73dc65
Elvandro <Elvisamuni22@gmail.com>
2022-04-11 06:38:44 +0300

    Add login page

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,110 @@
+<template>
+<div>
+  <div class="login-page">
+    <transition name="fade">
+      <div class="wallpaper-login"></div>
+    </transition>
+    <div class="wallpaper-register"></div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-8 mx-auto">
+          <div class="card login">
+            <h1>Sign In</h1>
+            <form class="form-group">
+              <input type="email" class="form-control" placeholder="Email" required>
+              <input type="password" class="form-control" placeholder="Password" required>
+              <router-link class="btn btn-primary" to="/home">Submit</router-link>
+              <p>Don't have an account? <a href="#">Sign up here</a>
+              </p>
+              <p><a href="#">Forgot your password?</a></p>
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</div>
+</template>
+<style scoped>
+p {
+  line-height: 1rem;
+}
+
+.card {
+  padding: 20px;
+}
+
+.form-group input {
+  margin-bottom: 20px;
+
+}
+
+
+.login-page {
+  align-items: center;
+  display: flex;
+  height: 100vh;
+}
+
+
+.wallpaper-login {
+  background: url(https://images.pexels.com/photos/32237/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260) no-repeat center center;
+  background-size: cover;
+  height: 100%;
+  position: absolute;
+  width: 100%;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.wallpaper-register {
+  background: url(https://images.pexels.com/photos/533671/pexels-photo-533671.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260) no-repeat center center;
+  background-size: cover;
+  height: 100%;
+  position: absolute;
+  width: 100%;
+  z-index: -1;
+}
+
+h1 {
+  margin-bottom: 1.5rem;
+}
+
+.error {
+  animation-name: errorShake;
+  animation-duration: 0.3s;
+}
+
+@keyframes errorShake {
+  0% {
+    transform: translateX(-25px);
+  }
+
+  25% {
+    transform: translateX(25px);
+  }
+
+  50% {
+    transform: translateX(-25px);
+  }
+
+  75% {
+    transform: translateX(25px);
+  }
+
+  100% {
+    transform: translateX(0);
+  }
+}
+</style>
